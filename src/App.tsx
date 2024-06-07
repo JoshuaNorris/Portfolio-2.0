a7f5e25307528a97aef70fde18acea14c80f32c9
Joshua Norris <joshua.norris52@gmail.com>
2024-06-07 12:51:34 -0500

    adding layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,17 @@ import './App.css'
 const App:React.FC = () => {
 
   return (
-    <>
-    </>
+    <section className='homepagegrid'>
+      <div className='sidebar'>1</div>
+      <div className='content'>
+        <div className='contentflexbox'>
+          <div className='title'>Josh Norris</div>
+          <div className='infogrids'>Josh Norris</div>
+          <div className='contacticons'>Josh Norris</div>
+        </div>
+      </div>
+      <div className='sidebar'>3</div>
+    </section>
   );
 };
 
